c3aabbf9e2f8890096f5e904971e1c0900add7bb
Adriano Luis Machado Abreu <adriano.luis.machado2013@hormail.com>
2020-06-11 15:30:35 -0300

    Obtendo um post por id

---
+++ src/main/java/com/adrianoabreu/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.adrianoabreu.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adrianoabreu.workshopmongo.domain.Post;
+import com.adrianoabreu.workshopmongo.repository.PostRepository;
+import com.adrianoabreu.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
